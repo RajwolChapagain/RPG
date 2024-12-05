15584241c3508a3e4ec3d5cd74fa9619b63d4bb9
Rajwol Chapagain <rajwolchapagain@gmail.com>
2024-12-04 20:36:33 -0500

    Break functionality down into pure functions

--- scripts/party.gd
+++ scripts/party.gd
@@ -5,11 +5,17 @@ extends Node2D
 var party_members = []
 
 func _ready() -> void:
+	instantiate_characters_and_add_to_list(character_scenes, party_members)
+	establish_queue(party_members)
+	activate_party_member(0)
+	
+func instantiate_characters_and_add_to_list(character_scenes, party_members):
 	for scene in character_scenes:
 		var character = scene.instantiate()
 		add_child(character)
 		party_members.append(character)
-
+		
+func establish_queue(party_members):
 	for i in range(0, len(party_members)):
 		if i == len(party_members) - 1:
 			party_members[i].next_character = party_members[0]
@@ -17,8 +23,13 @@ func _ready() -> void:
 			party_members[i].next_character = party_members[i + 1]
 			
 		party_members[i].character_moved.connect(on_character_moved)
-		
-	party_members[0].is_active = true
+	
+func activate_party_member(index: int):
+	for i in range(0, len(party_members)):
+		if i == index:
+			party_members[index].is_active = true
+		else:
+			party_members[i].is_active = false
 	
 func on_character_moved(character, old_grid_pos):
 	if not character.next_character.is_active:
