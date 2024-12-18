14999ce31167624a25155117f7918b4b11eb9616
Rajwol Chapagain <rajwolchapagain@gmail.com>
2024-12-18 16:00:55 -0600

    Create grid position tracker

---
+++ scripts/grid_position_tracker.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name GridPositionTracker
+
+@export var grid_size: int = 32
+@onready var grid_coord: Vector2i = global_position / grid_size
+
+func _physics_process(delta: float) -> void:
+	set_grid_coord(global_position / grid_size)
+	
+func set_grid_coord(new_coord: Vector2i) -> void:
+	grid_coord = new_coord
+	
+func get_grid_coord() -> Vector2i:
+	return grid_coord
