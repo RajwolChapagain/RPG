44db26f7972426388ce34a69fdf24231767354e0
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-01-08 21:07:37 -0500

    Account for move direction when calculating grid pos

--- scripts/grid_position_tracker.gd
+++ scripts/grid_position_tracker.gd
@@ -3,9 +3,15 @@ class_name GridPositionTracker
 
 @export var grid_size: int = 32
 @onready var grid_coord: Vector2i = global_position / grid_size
+@onready var position_last_frame: Vector2 = global_position
 
 func _physics_process(delta: float) -> void:
-	set_grid_coord(ceil(global_position / grid_size))
+	if global_position.x > position_last_frame.x || global_position.y > position_last_frame.y:
+		set_grid_coord(ceil(global_position / grid_size))
+	else:
+		set_grid_coord(floor(global_position / grid_size))
+		
+	position_last_frame = global_position
 	
 func set_grid_coord(new_coord: Vector2i) -> void:
 	grid_coord = new_coord
