2f6e84373ac9d8fa1d7590be0d93b55aa62421ed
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-01-11 18:46:49 -0500

    Emit player encountered signal

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal enemy_encountered_player(enemy)
+
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player_character"):
-		print("Player encountered!")
+		enemy_encountered_player.emit(self)
