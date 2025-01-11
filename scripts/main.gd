fa4bffb337507ad76d85efa034a1bc66d60e368c
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-01-11 18:47:21 -0500

    Listen and respond to player encountered signal

---
+++ scripts/main.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var battle_scene: PackedScene
+
+func _on_enemy_enemy_encountered_player(enemy: Variant) -> void:
+	var player_character_stats = []
+	for player_character in get_tree().get_nodes_in_group("player_character"):
+		player_character_stats.append(player_character.stats)
+	
+	var curr_battle_scene = battle_scene.instantiate()
+	curr_battle_scene.player_character_stats = player_character_stats
+	add_child(curr_battle_scene)
