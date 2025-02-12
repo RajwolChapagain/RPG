e9f88ea3bda4776cd3c644c9be5f4e60df9b2d0e
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-02-11 21:29:50 -0500

    Create base stats and use that in other stats

--- scripts/resource_scripts/enemy_stats.gd
+++ scripts/resource_scripts/enemy_stats.gd
@@ -1,6 +1,4 @@
 extends Resource
 class_name EnemyStats
 
-@export var attack_damage: int
-@export var hp: int
-@export var battle_sprite: Texture2D
+@export var base_stats : BaseStats
--- scripts/resource_scripts/player_stats.gd
+++ scripts/resource_scripts/player_stats.gd
@@ -4,6 +4,4 @@ class_name PlayerStats
 enum types { Archer, Cleric, Knight, Lady, Mage }
 
 @export var type: types
-@export var attack_damage: int
-@export var hp: int
-@export var battle_sprite: Texture2D
+@export var base_stats : BaseStats
