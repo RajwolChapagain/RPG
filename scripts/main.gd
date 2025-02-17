bd12258eac2fbf2b160a855edf98327de2acbe74
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-02-16 20:24:30 -0500

    Use BaseStats for enemy instead of EnemyStats

--- scripts/main.gd
+++ scripts/main.gd
@@ -6,9 +6,12 @@ func _on_enemy_enemy_encountered_player(enemy: Variant) -> void:
 	var player_character_stats : Array[BaseStats] = []
 	for player_character in get_tree().get_nodes_in_group("player_character"):
 		player_character_stats.append(player_character.stats)
-	
+		
+	var enemy_stats : Array[BaseStats] = []
+	enemy_stats.append(enemy.stats)
+		
 	var curr_battle_scene = battle_scene.instantiate()
 	curr_battle_scene.player_character_stats = player_character_stats
-	curr_battle_scene.enemy_stats = [enemy.stats]
+	curr_battle_scene.enemy_stats =  enemy_stats
 	curr_battle_scene.active_character_index = $Party.get_active_member_index()
 	add_child(curr_battle_scene)
