b7526f463f896f2e9bf731c8f3447ff95f9d9779
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-02-23 15:10:37 -0500

    Create selector scene

---
+++ scripts/target_select.gd
@@ -0,0 +1,37 @@
+extends Node
+
+@export var targets: Array[Node2D]
+@export var target_indicator: Texture2D
+@export var pointer_y_offset = 25
+var selected: int = 0:
+	get:
+		return selected
+	set(value):
+		selected = value
+		mark_selected(value)
+
+signal target_selected(target_index)
+
+func _ready() -> void:
+	mark_selected(selected)
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("select_target"):
+		target_selected.emit(selected)
+	
+	if event.is_action_pressed("select_next"):
+		select_next()
+	
+	if event.is_action_pressed("select_previous"):
+		select_previous()
+	
+func select_next() -> void:
+	selected = (selected + 1) % len(targets)
+
+func select_previous() -> void:
+	selected = (selected - 1) % len(targets)
+
+func mark_selected(index) -> void:
+	%Pointer.position.x = targets[index].position.x
+	%Pointer.position.y = targets[index].position.y - pointer_y_offset
+	%Pointer.visible = true
