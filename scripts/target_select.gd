cb9bf39590c5c78f6c2a7154b0ace55157f34fb5
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-02-23 15:52:25 -0500

    Implement activation logic

--- scripts/target_select.gd
+++ scripts/target_select.gd
@@ -1,8 +1,10 @@
 extends Node
 
-@export var targets: Array[Node2D]
+@export var targets : Array
 @export var target_indicator: Texture2D
 @export var pointer_y_offset = 25
+@export var active: bool = false
+
 var selected: int = 0:
 	get:
 		return selected
@@ -11,11 +13,11 @@ var selected: int = 0:
 		mark_selected(value)
 
 signal target_selected(target_index)
-
-func _ready() -> void:
-	mark_selected(selected)
 	
 func _input(event: InputEvent) -> void:
+	if not active:
+		return
+		
 	if event.is_action_pressed("select_target"):
 		target_selected.emit(selected)
 	
@@ -35,3 +37,17 @@ func mark_selected(index) -> void:
 	%Pointer.position.x = targets[index].position.x
 	%Pointer.position.y = targets[index].position.y - pointer_y_offset
 	%Pointer.visible = true
+
+func set_targets(new_targets) -> void:
+	targets = new_targets
+
+func activate() -> void:
+	assert(len(targets) != 0)
+	active = true
+	mark_selected(selected)
+	
+func deactivate() -> void:
+	targets.clear()
+	active = false
+	%Pointer.visible = false
+	
