afe48892840b0b168bde7a7e6afdb3ef0aa0826c
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-03-10 22:03:43 -0400

    Implement alternating dialogue logic

--- scripts/dialogue.gd
+++ scripts/dialogue.gd
@@ -2,19 +2,43 @@ extends Control
 
 @export var dialogues_1: Array[String]= []
 @export var dialogues_2: Array[String] = []
-var current_dialogue_1: int = 0
-var current_dialogue_2: int = 0
-var turn: int = 1
+var current_dialogue_index_1: int = 0
+var current_dialogue_index_2: int = 0
+var current_dialogues: Array[String]
+var current_dialogue_index: int = 0
+
+var turn: int:
+	get:
+		return turn
+	set(value):
+		current_dialogues = dialogues_1 if value == 1 else dialogues_2
+		current_dialogue_index = current_dialogue_index_1 if value == 1 else current_dialogue_index_2
+		turn = value
 
 func _ready() -> void:
-	%DialogueLabel.text = dialogues[current_dialogue]
+	turn = 1
+	update_dialogue_label()
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("advance_dialogue"):
-		current_dialogue += 1
-		current_dialogue = clamp(current_dialogue, 0, len(dialogues) - 1)
-		%DialogueLabel.text = dialogues[current_dialogue]
+		advance_dialogue()
+		
+func advance_dialogue() -> void:
+		current_dialogue_index += 1
+		if current_dialogues[current_dialogue_index].is_empty():
+			switch_turns()
+		update_dialogue_label()
 
+func switch_turns() -> void:
+	if turn == 1:
+		current_dialogue_index_1 = current_dialogue_index + 1
+		turn = 2
+	else:
+		current_dialogue_index_2 = current_dialogue_index +  1
+		turn = 1
+		
+	current_dialogue_index -= 1
+	advance_dialogue()
+		
 func update_dialogue_label() -> void:
-	var dialogues = dialogues_1 if turn == 1 else dialogues_2
-	var index = current_dialogue_1 if turn == 1 else current_dialogue_2
+	%DialogueLabel.text = current_dialogues[current_dialogue_index]
