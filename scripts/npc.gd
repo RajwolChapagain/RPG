edf8cdba2bf43f0b1980479c8af54559de1c468c
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-03-12 22:13:42 -0400

    Instantiate dialogue scene upon encountering player

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -1,7 +1,14 @@
 extends Area2D
 
-signal npc_encountered_player(npc)
-
+@export var DIALOGUE_SCENE: PackedScene = preload("res://scenes/dialogue.tscn")
+@export var my_dialogues: Dialoguer
+@export var player_dialogues: Dialoguer
+ 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player_character"):
-		npc_encountered_player.emit(self)
+		initiate_dialogue()
+
+func initiate_dialogue() -> void:
+	var dialogue_scene = DIALOGUE_SCENE.instantiate()
+	dialogue_scene.initialize(player_dialogues, my_dialogues)
+	get_tree().root.add_child(dialogue_scene)
