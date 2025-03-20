51ed121b5365d0ea27df15ebbf6c8ad963950328
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-03-19 22:14:11 -0400

    Add max_hp property to base stats and assign the same

--- scripts/resource_scripts/base_stats.gd
+++ scripts/resource_scripts/base_stats.gd
@@ -3,5 +3,11 @@ class_name BaseStats
 
 @export var name: String
 @export var attack_damage: int
-@export var hp: int
+@export var max_hp: int
+@export var hp: int:
+	get:
+		return hp
+	set(value):
+		hp = clamp(value, 0, max_hp)
+		
 @export var battle_sprite: Texture2D
