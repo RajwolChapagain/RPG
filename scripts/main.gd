182fbb6399d7c4a236d8509d47083a9996ced078
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-03-26 18:06:20 -0400

    Add battle scene to root instead of main

--- scripts/main.gd
+++ scripts/main.gd
@@ -15,13 +15,13 @@ func _on_enemy_enemy_encountered_player(enemy: Variant) -> void:
 	curr_battle_scene.enemy_stats =  enemy_stats
 	curr_battle_scene.first_attacker_index = $Party.get_active_member_index()
 	curr_battle_scene.battle_ended.connect(on_battle_ended)
-	add_child(curr_battle_scene)
+	get_tree().root.add_child(curr_battle_scene)
 	
 	for player in get_tree().get_nodes_in_group("player_character"):
 		player.set_active(false)
 
 func on_battle_ended() -> void:
-	get_node("BattleScene").queue_free()
+	get_tree().root.get_node("BattleScene").queue_free()
 	remove_dead_entities()
 	
 func remove_dead_entities() -> void:
