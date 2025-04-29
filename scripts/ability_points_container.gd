f78892f37025014d2b5c66e19a1ef552c3206edc
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-04-28 21:18:02 -0400

    Add APIs for increasing and decreasing points

--- scripts/ability_points_container.gd
+++ scripts/ability_points_container.gd
@@ -4,7 +4,32 @@ extends HFlowContainer
 @export var points: int = 2
 @export var ability_point_scene: PackedScene = load("res://scenes/ability_point.tscn")
 
+var ability_points = []
+
 func _ready() -> void:
 	for i in range(max_points):
 		var ability_point = ability_point_scene.instantiate()
 		add_child(ability_point)
+		ability_points.append(ability_point)
+	
+	play_activate_animation(0, points)
+	
+func increase_points(value: int) -> void:
+	var old_points = points
+	points += value
+	points = clampi(points, 0, max_points)
+	play_activate_animation(old_points, points - old_points)
+
+func decrease_points(value: int) -> void:
+	var old_points = points
+	points -= value
+	points = clampi(points, 0, max_points)
+	play_deactivate_animation(points, old_points - points)
+
+func play_activate_animation(start_index: int, count: int) -> void:
+	for i in range(start_index, start_index + count):
+		ability_points[i].get_node("AnimationPlayer").play("activate")
+
+func play_deactivate_animation(start_index, count: int) -> void:
+	for i in range(start_index, start_index + count):
+		ability_points[i].get_node("AnimationPlayer").play("deactivate")
