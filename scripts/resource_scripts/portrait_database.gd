105835fe4c9bc24a7c4574ee2324639903a41bb6
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-05-09 13:21:38 -0400

    Specify Dictionary type

--- scripts/resource_scripts/portrait_database.gd
+++ scripts/resource_scripts/portrait_database.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name PortraitDatabase
 
-@export var sprite_dictionary: Dictionary
+@export var sprite_dictionary: Dictionary[String, Texture2D]
 
 func get_portrait(name: String):
 	if not sprite_dictionary.has(name):
