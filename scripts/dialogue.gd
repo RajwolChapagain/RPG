ceaff37d5089b94e8578af8a49241c118eba3c4f
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-05-09 11:40:07 -0400

    Add methods to dim and brighten portraits

--- scripts/dialogue.gd
+++ scripts/dialogue.gd
@@ -8,7 +8,13 @@ func enable_left_sprite() -> void:
 
 func disable_left_sprite() -> void:
 	%Portrait1.visible = false
-		
+	
+func dim_left_sprite() -> void:
+	%Portrait1.modulate = Color.from_hsv(%Portrait1.modulate.h, %Portrait1.modulate.s, 0.5);
+	
+func brighten_left_sprite() -> void:
+	%Portrait1.modulate = Color.from_hsv(%Portrait1.modulate.h, %Portrait1.modulate.s, 1.0);
+
 func load_right_sprite(sprite: Texture2D) -> void:
 	%Portrait2.texture = sprite
 
@@ -17,3 +23,9 @@ func enable_right_sprite() -> void:
 
 func disable_right_sprite() -> void:
 	%Portrait2.visible = false
+
+func dim_right_sprite() -> void:
+	%Portrait2.modulate = Color.from_hsv(%Portrait2.modulate.h, %Portrait2.modulate.s, 0.5);
+
+func brighten_right_sprite() -> void:
+	%Portrait2.modulate = Color.from_hsv(%Portrait2.modulate.h, %Portrait2.modulate.s, 1.0);
