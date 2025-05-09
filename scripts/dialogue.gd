c3bddf564f58045f381015143237c01c184e1db3
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-05-09 11:47:04 -0400

    Add methods to set dialogue

--- scripts/dialogue.gd
+++ scripts/dialogue.gd
@@ -1,5 +1,13 @@
 extends Control
 
+#region: Left
+func set_left_dialogue(sprite:Texture2D, text: String) -> void:
+	load_left_sprite(sprite)
+	brighten_left_sprite()
+	dim_right_sprite()
+	enable_left_sprite()
+	set_dialogue_text(text)
+	
 func load_left_sprite(sprite: Texture2D) -> void:
 	%Portrait1.texture = sprite
 	
@@ -14,7 +22,16 @@ func dim_left_sprite() -> void:
 	
 func brighten_left_sprite() -> void:
 	%Portrait1.modulate = Color.from_hsv(%Portrait1.modulate.h, %Portrait1.modulate.s, 1.0);
+#endregion
 
+#region: Right
+func set_right_dialogue(sprite:Texture2D, text: String) -> void:
+	load_right_sprite(sprite)
+	brighten_right_sprite()
+	dim_left_sprite()
+	enable_right_sprite()
+	set_dialogue_text(text)
+	
 func load_right_sprite(sprite: Texture2D) -> void:
 	%Portrait2.texture = sprite
 
@@ -29,3 +46,7 @@ func dim_right_sprite() -> void:
 
 func brighten_right_sprite() -> void:
 	%Portrait2.modulate = Color.from_hsv(%Portrait2.modulate.h, %Portrait2.modulate.s, 1.0);
+#endregion
+
+func set_dialogue_text(text: String) -> void:
+	%DialogueLabel.text = text
