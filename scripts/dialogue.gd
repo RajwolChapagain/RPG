cf82688f7e78d1cb66646d25d2ba4ad7909a2233
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-05-09 11:34:37 -0400

    Add methods to enable and disable portraits

--- scripts/dialogue.gd
+++ scripts/dialogue.gd
@@ -3,5 +3,17 @@ extends Control
 func load_left_sprite(sprite: Texture2D) -> void:
 	%Portrait1.texture = sprite
 	
+func enable_left_sprite() -> void:
+	%Portrait1.visible = true
+
+func disable_left_sprite() -> void:
+	%Portrait1.visible = false
+		
 func load_right_sprite(sprite: Texture2D) -> void:
 	%Portrait2.texture = sprite
+
+func enable_right_sprite() -> void:
+	%Portrait2.visible = true
+
+func disable_right_sprite() -> void:
+	%Portrait2.visible = false
