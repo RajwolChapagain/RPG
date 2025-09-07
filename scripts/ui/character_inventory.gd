bbb75e0f621f5d3a9ce95ef20a321088d749466f
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-09-07 15:20:21 -0400

    Add character inventory scenes to inv manager

--- scripts/ui/character_inventory.gd
+++ scripts/ui/character_inventory.gd
@@ -1,13 +1,24 @@
 extends Control
 
-@export var character_name: String
-@export var portrait: Texture2D
-@export var equipped_items: Array[Item]
-
-var activated_slots = 2
+var activated_slots = 1
 const MAX_SLOTS = 4
 
 func _ready() -> void:
 	for i in range(MAX_SLOTS):
 		if i < activated_slots:
 			%SlotContainer.get_child(i).activate()
+
+func initialize(character_name: String, portrait: Texture2D) -> void:
+	%Portrait.texture = portrait
+	%CharacterNameLabel.text = character_name
+	
+func activate_new_slot() -> void:
+	if activated_slots >= MAX_SLOTS:
+		push_error('Error: Max number of inventory slots already activated for character %s' % %CharacterNameLabel.text)
+		return
+		
+	%SlotContainer.get_child(activated_slots).activate()
+	activated_slots += 1
+
+func equip_item_to_slot(item: Item, index: int) -> Item:
+	return %SlotContainer.get_child(index).equip_item(item)
