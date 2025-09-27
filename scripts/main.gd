4878014602b9d18e7f6ee29f1b6bfd27f6b167cc
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-09-27 14:56:59 -0400

    Drop essence of unselected hero

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,6 +5,7 @@ extends Node
 @export var levels: Array[PackedScene]
 @export var party_scene: PackedScene
 @export var character_name_to_scene: Dictionary[String, PackedScene]
+@export var character_name_to_base_stats: Dictionary[String, BaseStats]
 
 var current_level: Node = null
 var party: Party = null
@@ -48,3 +49,10 @@ func on_characters_selected(names: Array[String]):
 	add_child(party)
 	%PauseMenu.initialize_inventory()
 	load_next_level()
+	drop_unselected_player_essence(names)
+	
+func drop_unselected_player_essence(selected_players: Array[String]) -> void:
+	var all_players = ['Rachelle', 'Magda', 'Josephine', 'Lachlan', 'Einar']
+	var missing_player = all_players.filter(func(player_name): return player_name not in selected_players)[0]
+	ItemDropManager.drop_items([GameManager.crystalize_stat(character_name_to_base_stats[missing_player])])
+	
