b552e094bc5bd1e49857a053e3a60949a5d2e068
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-10-06 23:16:05 -0400

    Create main menu states

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,12 +1,33 @@
 extends Node
 
 @export var main_scene: PackedScene
+var state: states = states.TITLE:
+	get():
+		return state
+	set(value):
+		if value == states.TITLE:
+			for panel in %ContentPanel.get_children():
+				panel.visible = false
+		elif value == states.PLAY:
+			%ContentPanel.visible = true
+			for panel in %ContentPanel.get_children():
+				panel.visible = false
+			%PlayPanel.visible = true
+		elif value == states.SETTINGS:
+			%ContentPanel.visible = true
+			for panel in %ContentPanel.get_children():
+				panel.visible = false
+			%SettingsPanel.visible = true
+		state = value
+		
+enum states { TITLE, PLAY, SETTINGS }
 
 func _ready() -> void:
 	connect_save_slot_signals()
+	%PlayButton.grab_focus()
 	
 func connect_save_slot_signals() -> void:
-	for save_slot: SaveSlot in %SlotsContainer.get_children():
+	for save_slot: SaveSlot in %PlayPanel.get_children():
 		save_slot.load_game_button_pressed.connect(load_saved_game)
 		save_slot.new_game_button_pressed.connect(load_new_game)
 		
@@ -29,4 +50,13 @@ func load_saved_game(saved_slot_id: int) -> void:
 	queue_free()
 	
 func _on_play_button_pressed() -> void:
-	pass
+	if state == states.PLAY:
+		state = states.TITLE
+	else:
+		state = states.PLAY
+
+func _on_settings_button_pressed() -> void:
+	if state == states.SETTINGS:
+		state = states.TITLE
+	else:
+		state = states.SETTINGS
