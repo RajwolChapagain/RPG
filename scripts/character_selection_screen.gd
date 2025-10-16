fdb2ffb6c24c15da7c7e01baedefa28506701481
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-10-15 20:39:12 -0400

    Place rock beneath unselected character

--- scripts/character_selection_screen.gd
+++ scripts/character_selection_screen.gd
@@ -54,8 +54,19 @@ func finalize_selection() -> void:
 	var character_names: Array[String] = []
 	for index in selected_indices:
 		character_names.append(character_stats[index].name)
+	for button in %Buttons.get_children():
+		button.release_focus()
+		
+	slide_stat_card_out()
+	make_buttons_invisible()
+	%AnimationPlayer.play("label_slide_out")
+	await play_character_orient_animation()
+	place_rock()
+	%AnimationPlayer.play("camera_follow")
+	await play_landing_animation()
+	%SharpRock.play("flow_blood")
 	characters_selected.emit(character_names)
-	queue_free()
+	#queue_free()
 
 func _on_button_1_focus_entered() -> void:
 	update_stats_card(0)
@@ -82,3 +93,33 @@ func play_shroud_animation() -> void:
 	tween.tween_property(%Shroud, 'modulate', Color(%Shroud.modulate.r, %Shroud.modulate.g, %Shroud.modulate.b, 0), 2).set_ease(Tween.EaseType.EASE_IN_OUT)
 	await tween.finished
 	await get_tree().create_timer(3).timeout
+
+func play_character_orient_animation() -> void:
+	var tween = get_tree().create_tween()
+	tween.set_parallel(true)
+	for button in %Buttons.get_children():
+		tween.tween_property(button.get_child(0), 'rotation', 0, 2)
+	await tween.finished
+
+func slide_stat_card_out() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(%StatCard, 'position', Vector2(%StatCard.position.x + 60, %StatCard.position.y), 1).set_ease(Tween.EASE_OUT)
+	await tween.finished
+
+func make_buttons_invisible() -> void:
+	for button: Button in %Buttons.get_children():
+		button.self_modulate = Color.TRANSPARENT
+		 
+func play_landing_animation() -> void:
+	var tween = get_tree().create_tween()
+	tween.set_parallel(true)
+	var i = 0
+	for button in %Buttons.get_children():
+		var final_position = %FinalPositionMarkers.get_child(i).global_position
+		tween.tween_property(button.get_child(0), 'global_position', final_position, 2).set_ease(Tween.EASE_IN)
+		i += 1
+	await tween.finished
+	
+func place_rock() -> void:
+	var unselected_index = range(5).filter(func (x): return x not in selected_indices)[0]
+	%SharpRock.global_position = %FinalPositionMarkers.get_child(unselected_index).global_position - Vector2(0, 8)
