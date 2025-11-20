913f80dea700e98abd41b3cb7201cdcb3ebc9e2a
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-11-19 21:24:14 -0500

    Create walls for testing

--- scripts/toy_scene.gd
+++ scripts/toy_scene.gd
@@ -1,9 +1,1 @@
 extends Node
-
-func _ready() -> void:
-	#ItemDropManager.drop_items([load("res://resources/items/doloris's_necklace.tres"), Item.new("Consumable", Item.ItemType.CONSUMABLE, [StatModifier.new("max_hp", "*", false, 2), StatModifier.new("attack_damage", '-', true, 50)])])
-	%PauseMenu.initialize_inventory()
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
-		pass
