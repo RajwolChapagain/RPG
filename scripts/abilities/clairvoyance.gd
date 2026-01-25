40343d493ee8909f59f945f687b34a830b3e43e1
Rajwol Chapagain <rajwolchapagain@gmail.com>
2026-01-24 23:18:46 -0500

    Defer call to confirm button grab focus

--- scripts/abilities/clairvoyance.gd
+++ scripts/abilities/clairvoyance.gd
@@ -11,7 +11,9 @@ func execute(_caster: Battler, target: Battler) -> void:
 func show_stats(stats: BaseStats) -> void:
 	%StatCard.portraits.sprite_dictionary[stats.name] = stats.battle_sprite
 	%StatCard.stats = stats
-	%ConfirmButton.grab_focus()
+	# Can't call grab_focus immediate because !is_inside_tree() error pops up
+	var grab_focus_callable: Callable = Callable(%ConfirmButton, 'grab_focus')
+	grab_focus_callable.call_deferred()
 
 func _on_confirm_button_button_down() -> void:
 	stats_panel_closed.emit()
