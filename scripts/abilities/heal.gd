be75b90f6677ee7b158b8cb68e1106bc1ba947b1
Rajwol Chapagain <rajwolchapagain@gmail.com>
2026-01-31 22:47:16 -0500

    Heal ability: flat amount -> proportional based on max-hp

--- scripts/abilities/heal.gd
+++ scripts/abilities/heal.gd
@@ -1,11 +1,11 @@
 class_name Heal extends Ability
 
-@export var heal: int
+@export_range(0, 100) var heal_percentage: float
 
 func execute(_caster: Battler, target: Battler) -> void:
 	%HealParticles.global_position = target.global_position
 	%HealParticles.emitting = true
-	target.stats.hp += heal
+	target.stats.hp += int(heal_percentage / 100 * target.stats.max_hp)
 	await %HealParticles.finished
 	ability_finished_execution.emit()
 	queue_free()
