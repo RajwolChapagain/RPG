78468c495f0073355469b78c2801fb43915b146b
Rajwol Chapagain <rajwolchapagain@gmail.com>
2026-02-03 21:57:27 -0500

    Override ability info button theme to be transparent

--- scripts/ui/ability_card.gd
+++ scripts/ui/ability_card.gd
@@ -1,12 +1,16 @@
 extends Panel
 
 signal ability_info_button_pressed
+signal ability_info_button_released
 
 func INITIALIZE(ability_name: String, ability_image: Texture2D, ability_description: String) -> void:
 	%AbilityName.text = ability_name
 	if ability_image:
 		%AbilityImage.texture = ability_image
 	%AbilityDescription.text = ability_description
-
-func _on_button_pressed() -> void:
+	
+func _on_button_focus_entered() -> void:
 	ability_info_button_pressed.emit(%AbilityName.text)
+
+func _on_button_focus_exited() -> void:
+	ability_info_button_released.emit(%AbilityName.text)
