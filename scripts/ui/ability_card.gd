a054e06fd3d27cb24446d7ea7c254c451f7b0e39
Rajwol Chapagain <rajwolchapagain@gmail.com>
2026-02-03 21:40:05 -0500

    Add button to ability card and reduce size

--- scripts/ui/ability_card.gd
+++ scripts/ui/ability_card.gd
@@ -1,7 +1,12 @@
 extends Panel
 
+signal ability_info_button_pressed
+
 func INITIALIZE(ability_name: String, ability_image: Texture2D, ability_description: String) -> void:
 	%AbilityName.text = ability_name
 	if ability_image:
 		%AbilityImage.texture = ability_image
 	%AbilityDescription.text = ability_description
+
+func _on_button_pressed() -> void:
+	ability_info_button_pressed.emit(%AbilityName.text)
