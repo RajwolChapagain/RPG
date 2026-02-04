3bd755c0098039b5ba3339e747c03d02d42a43d8
Rajwol Chapagain <rajwolchapagain@gmail.com>
2026-02-03 21:34:15 -0500

    Create ability image injection system

--- scripts/ui/ability_info_panel.gd
+++ scripts/ui/ability_info_panel.gd
@@ -1,6 +1,7 @@
 extends Panel
 
 @export var ability_card_scene: PackedScene
+@export var ability_images: Dictionary[String, Texture2D]
 
 func _on_visibility_changed() -> void:
 	if visible:
@@ -14,6 +15,9 @@ func populate_abilities() -> void:
 		for ability_scene: PackedScene in player.stats.abilities:
 			var ability_card = ability_card_scene.instantiate()
 			var ability: Ability = ability_scene.instantiate()
-			ability_card.INITIALIZE(ability.ability_name, null, ability.description)
+			var ability_image = null
+			if ability.ability_name in ability_images:
+				ability_image = ability_images[ability.ability_name]
+			ability_card.INITIALIZE(ability.ability_name, ability_image, ability.description)
 			%GridContainer.add_child(ability_card)
 			ability.queue_free()
