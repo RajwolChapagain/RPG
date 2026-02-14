4b592c1308f3a94b8ab4f8b05caa4b1714fe2407
Rajwol Chapagain <rajwolchapagain@gmail.com>
2026-02-14 10:50:36 -0500

    Animate patrolling enemy according to velocity

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,10 +5,36 @@ class_name Enemy
 @export var dropped_items: Array[Item] # Used by battle_manager before queuing this enemy for death
 @export var random_drop_item_count: int = 1 # Used by battle_manager before queuing this enemy for death
 @export_range(0, 100, 1) var random_dropped_item_rarity_modifier: float = 0 # Used by battle_manager before queuing this enemy for death
+var last_position
 
 @warning_ignore("unused_signal")
 signal enemy_defeated # Emitted by BattleManager at end of battle
 
+func _ready() -> void:
+	last_position = position
+	
+func _process(delta: float) -> void:
+	var movement = position - last_position
+	
+	# Ignore tiny movement (prevents jitter)
+	if movement.length() < 0.1:
+		return
+	
+	if abs(movement.x) > abs(movement.y):
+		# Horizontal dominant
+		if movement.x > 0:
+			%Sprite2D.play("move_right")
+		else:
+			%Sprite2D.play("move_left")
+	else:
+		# Vertical dominant
+		if movement.y > 0:
+			%Sprite2D.play("move_down")
+		else:
+			%Sprite2D.play("move_up")
+	
+	last_position = global_position
+	
 func get_gang() -> Array[BaseStats]:
 	return gang
 
