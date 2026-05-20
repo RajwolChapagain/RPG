37991d1d3fb94f269f18f39e2f836ae341b236a5
Rajwol Chapagain <rajwolchapagain@gmail.com>
2026-05-20 18:52:05 -0400

    Begin level 2 with unshroud animation

--- scripts/levels/level_2.gd
+++ scripts/levels/level_2.gd
@@ -2,6 +2,12 @@ extends Level
 
 func _ready() -> void:
 	MusicManager.play_music('level2')
+	play_unshroud_animation()
 
 func _on_boss_enemy_enemy_defeated() -> void:
 	level_completed.emit(level_number)
+	
+func play_unshroud_animation() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(%Shroud, 'modulate', Color(%Shroud.modulate, 0), 1).set_ease(Tween.EASE_OUT)
+	await tween.finished
