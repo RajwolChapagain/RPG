41ce8346fdbdbe306ec2f0094f83825387bc02c6
Rajwol Chapagain <rajwolchapagain@gmail.com>
2026-05-23 15:56:48 -0400

    Resolve #130

--- scripts/ui/pause/item_stats_panel.gd
+++ scripts/ui/pause/item_stats_panel.gd
@@ -1,36 +1,67 @@
 class_name ItemStatsPanel
-extends Control
+extends Panel
 
 @export var current_item: Item
 
 func _ready() -> void:
 	if current_item != null:
-		populate_label()
+		populate()
+		await get_tree().process_frame
+		reset_size()
+		size = %MarginContainer.get_combined_minimum_size()
 		
-func populate_label() -> void:
-	var out = ''
-	out += 'Type: %s\n' % Item.ItemType.keys()[current_item.type]
+func populate() -> void:
+	%TypeLabel.text = 'Type: %s' % Item.ItemType.keys()[current_item.type]
 	
 	var num_stats = len(current_item.stats)
 	for i in range(num_stats):
 		var stat_modifier: StatModifier = current_item.stats[i]
+
+		var out = ''
 		out += stat_modifier.modification
-		out += str(stat_modifier.amount)
+		out += str(stat_modifier.amount as int)
 		if stat_modifier.percentage:
 			out += '%'
-		out += ' %s' % stat_modifier.stat_name
-		out += '\n'
+			
+		var stat_name = stat_modifier.stat_name
+		
+		if stat_name == 'ap_slots':
+			%APSlotsRow.visible = true
+			%APSlotsLabel.text = out
+			continue
+		
+		if stat_name == 'hp':
+			out += ' HP'
+		elif stat_name == 'max_hp':
+			out += ' MAX'
+		elif stat_name == 'attack_damage':
+			out += ' ATK'
+		elif stat_name == 'defence':
+			out += ' DEF'
+		elif stat_name == 'dodge':
+			out += ' EVA'
+		elif stat_name == 'accuracy':
+			out += ' ACC'
+		elif stat_name == 'crit':
+			out += ' CRT'
+		elif stat_name == 'ap_per_attack':
+			out += ' AP'
+
+		var label: Label = Label.new()
+		label.text = out
+		label.add_theme_font_size_override('font_size', 6)
+		%HFlowContainer.add_child(label)
 		
 	if len(current_item.abilities) != 0:
-		out += 'Abilities: '
+		%AbilitiesColumn.visible = true
 		
+	var abilities_text = ''
 	var i = 0
 	for ability: PackedScene in current_item.abilities:
 		var temp_ability = ability.instantiate()
-		out += temp_ability.ability_name
-		if i != len(current_item.abilities) - 1:
-			out += ', '
-		i += 1
+		abilities_text += '- %s' % temp_ability.ability_name
 		temp_ability.queue_free()
+		if i != len(current_item.abilities) - 1:
+			abilities_text += '\n'
 	
-	%Label.text = out
+	%AbilitiesLabel.text = abilities_text
--- scripts/ui/pause/stat_card.gd
+++ scripts/ui/pause/stat_card.gd
@@ -2,7 +2,6 @@ extends Control
 class_name StatCard
 
 @export var portrait: TextureRect
-@export var stat_values: Control
 
 @export var stats: BaseStats:
 	get:
@@ -24,28 +23,45 @@ func initialize_ui() -> void:
 	update_stats_ui()
 	
 func update_stats_ui() -> void:
-	var stat_names = ['name', 'hp', 'attack_damage', 'defence', 'dodge', 'accuracy', 'crit', 'ap_per_attack', 'ap_slots' , 'abilities']
+	var stat_names = ['name', 'hp', 'max_hp', 'attack_damage', 'defence', 'dodge', 'accuracy', 'crit', 'ap_per_attack', 'ap_slots' , 'abilities']
 	
-	var i = 0
 	for stat_name in stat_names:
 		var label_text = ''
 		
-		if stat_name == 'hp':
-			label_text = '%s/%s' % [str(stats.hp), str(stats.max_hp)]
-		elif stat_name == 'abilities':
+		if stat_name == 'abilities':
 			var j = 0
 			for ability in stats.abilities:
 				var temp_ability = ability.instantiate()
-				label_text += temp_ability.ability_name
+				label_text += '- ' + temp_ability.ability_name
 				if j != len(stats.abilities) - 1:
-					label_text += ', '
+					label_text += '\n'
 				j += 1
 				temp_ability.queue_free()
 		else:
 			label_text = str(stats.get(stat_name))
 			
-		stat_values.get_child(i).text = label_text
-		i += 1
-		
+		if stat_name == 'name':
+			%NameLabel.text = label_text
+		elif stat_name == 'hp':
+			%HPLabel.text = label_text
+		elif stat_name == 'max_hp':
+			%MAXLabel.text = label_text
+		elif stat_name == 'attack_damage':
+			%ATKLabel.text = label_text
+		elif stat_name == 'defence':
+			%DEFLabel.text = label_text
+		elif stat_name == 'dodge':
+			%EVALabel.text = label_text
+		elif stat_name == 'accuracy':
+			%ACCLabel.text = label_text
+		elif stat_name == 'crit':
+			%CRTLabel.text = label_text
+		elif stat_name == 'ap_per_attack':
+			%APLabel.text = label_text
+		elif stat_name == 'ap_slots':
+			%APSlotsLabel.text = label_text
+		elif stat_name == 'abilities':
+			%AbilitiesLabel.text = label_text
+			
 func get_character_name() -> String:
 	return stats.name
