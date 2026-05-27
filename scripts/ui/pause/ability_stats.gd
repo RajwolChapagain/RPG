61cbf1fd3a24cf200fd391a95fac03591ee90535
Rajwol Chapagain <rajwolchapagain@gmail.com>
2026-05-27 19:16:54 -0400

    Add ability descriptions

--- scripts/ui/pause/ability_stats.gd
+++ scripts/ui/pause/ability_stats.gd
@@ -10,17 +10,18 @@ func set_stats(ability_name: String, items: Dictionary[String, String]) -> void:
 	for item in items:
 		var value = items[item]
 		var key_label = Label.new()
-		key_label.set_text(item)
+		key_label.set_text('%s:' % item)
 		key_label.add_theme_font_size_override('font_size', label_font_size)
-		%KeyContainer.add_child(key_label)
+		key_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
 		var value_label = Label.new()
-		value_label.set_text(': %s' % value)
+		value_label.set_text(value)
 		value_label.add_theme_font_size_override('font_size', label_font_size)
-		%ValueContainer.add_child(value_label)
+		value_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+		var row = HBoxContainer.new()
+		%StatsContainer.add_child(row)
+		row.add_child(key_label)
+		row.add_child(value_label)
 		
 func clear_existing_stats() -> void:
-	for label in %KeyContainer.get_children():
-		label.queue_free()
-	
-	for label in %ValueContainer.get_children():
-		label.queue_free()
+	for item in %StatsContainer.get_children():
+		item.queue_free()
