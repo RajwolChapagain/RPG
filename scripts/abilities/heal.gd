0f99809cbc5639937a42d7d4ac723943204ca084
Rajwol Chapagain <rajwolchapagain@gmail.com>
2026-05-27 16:13:23 -0400

    Balance heal ability

--- scripts/abilities/heal.gd
+++ scripts/abilities/heal.gd
@@ -1,12 +1,12 @@
 class_name Heal extends Ability
 
-@export_range(0, 100) var heal_percentage: float
+@export var heal_amount: int
 @export var heal_particles: Node
 
 func execute(_caster: Battler, target: Battler) -> void:
 	heal_particles.global_position = target.global_position
 	heal_particles.emitting = true
-	target.stats.hp += int(heal_percentage / 100 * target.stats.max_hp)
+	target.stats.hp += heal_amount
 	await heal_particles.finished
 	ability_finished_execution.emit()
 	queue_free()
@@ -14,5 +14,5 @@ func execute(_caster: Battler, target: Battler) -> void:
 func get_stats_dict() -> Dictionary[String, String]:
 	return {
 		'Cost': "%d Abilty %s" % [cost, "Point" if cost == 1 else "Points"],
-		'Heal Amount': "%d%% of Max HP" % heal_percentage,
+		'Heal Amount': "%d HP" % heal_amount,
 	}
